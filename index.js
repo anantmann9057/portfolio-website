51f5380a6e1254ffe3b5fad3c03620c2b0220760
Anant Kumar Mann <37663597+anantmann9057@users.noreply.github.com>
2024-11-29 20:51:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-
 var isMobile =
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    );
+  /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+    navigator.userAgent
+  );
 
-  if (isMobile) {
-    document.querySelector(".contact").href = "tel:+919001155788";
-  } else {
-    document.querySelector(".contact").href = "mailto:anantmann9057@gmail.com";
-  }
+if (isMobile) {
+  document.querySelector(".contact").href = "tel:+919001155788";
+} else {
+  document.querySelector(".contact").href = "mailto:anantmann9057@gmail.com";
+}
 
+
+$(document).ready(function () {
+});
+
+$('button').mouseover(function () { 
+  $('button').animate({opacity:1})
+});
+
+$('button').mouseout(function () { 
+  $('button').animate({opacity:0.5})
+});
